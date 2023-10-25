f1446ad44d88d4a7602fab0690352baa1c956575
irolram <rrivan@iesrafaelalberti.es>
2023-10-25 18:53:56 +0200

    Primera versión

---
+++ scr/ejercicio5.py
@@ -0,0 +1,12 @@
+"""Escribe un programa que pida el importe sin IVA de un artículo y el tipo de IVA a 
+aplicar y calcule e imprima por pantalla el precio final del artículo.
+"""
+
+num1 = int(input("Precio primer producto: "))
+print(num1)
+num2 = int(input("Precio segundo producto: "))
+print(num2)
+iva = 0.21
+suma = num1 + num2
+
+
