37a6d178f9ff9c2b7bcf55599cdcd35a5fe11d06
irolram <rrivan@iesrafaelalberti.es>
2023-11-02 21:36:10 +0100

    24 hecho

--- scr/ejercicio24.py
+++ scr/ejercicio24.py
@@ -3,6 +3,6 @@ Escribir un programa que pregunte por consola el precio de un producto en euros 
 dos decimales y muestre por pantalla el número de euros y el número de céntimos del
 precio introducido.
 """
-precio = float(input("Precio del producto: \n"))
-euros =
-cent =
+precio = input("Introduce el precio del producto con decimales:  ")
+print(precio[:precio.find('.')], "euros y" , precio[precio.find(".")+1:], "céntimos.") 
+#el +1 es para quitar el punto
